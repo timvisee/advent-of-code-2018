7df4e50cb681faea72aad9e8191b06f31e3fc9c7
timvisee <timvisee@gmail.com>
2018-12-28 14:36:56 +0100

    Simplify day01a

--- day01a/src/main.rs
+++ day01a/src/main.rs
@@ -1,12 +1,11 @@
 use std::fs::read_to_string;
 
 fn main() {
-    // Read the input
-    let input = read_to_string("input.txt").expect("failed to read input file");
-
     println!(
         "Frequency: {}",
-        input.lines()
+        read_to_string("input.txt")
+            .expect("failed to read input file")
+            .lines()
             .map(|v| v.parse::<i32>().unwrap())
             .fold(0, |a, b| a + b),
     );
